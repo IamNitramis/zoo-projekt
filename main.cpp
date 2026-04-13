6bfd4f9e013f64b0f726ef07b5f979ed4db535d9
iamnitramis <martin2gab@gmail.com>
2026-04-13 15:42:40 +0200

    testovaci commit again

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
 
     std::cout << "Ahoj kuci, funguje to :)) !" << std::endl;
-    std::cout << "Test commit od Martina";
+    std::cout << "Test commit od Martina cislo 2";
 
     return 0;
 }
